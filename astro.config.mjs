892913822bbae7ff53ce283c0c741ffdae4764b6
StJohn <stjohn@me.com>
2025-05-13 10:34:47 +0100

    Add Capo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        weights: [400],
+        styles: ["normal"],
+        subsets: ["latin"],
+        cssVariable: "--font-roboto",
+      },
+    ],
+    responsiveImages: true,
+  },
   site: "http://localhost/",
   integrations: [sitemap()],
 });
