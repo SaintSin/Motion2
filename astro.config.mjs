1b5903436362937b78db58377ce2ee6d4a5467f1
StJohn <stjohn@me.com>
2025-10-26 09:26:01 +0000

    Add biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,36 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig, fontProviders } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
-import playformCompress from "@playform/compress";
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Roboto",
-        weights: [400],
-        styles: ["normal"],
-        subsets: ["latin"],
-        cssVariable: "--font-roboto",
-      },
-    ],
-  },
-  image: {
-    responsiveStyles: true,
-  },
-  integrations: [
-    sitemap(),
-    (await import("@playform/compress")).default({
-      CSS: true,
-      HTML: false,
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-    }),
-  ],
-  site: "http://localhost/",
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: 'Roboto',
+				weights: [400],
+				styles: ['normal'],
+				subsets: ['latin'],
+				cssVariable: '--font-roboto',
+			},
+		],
+	},
+	image: {
+		responsiveStyles: true,
+	},
+	integrations: [
+		sitemap(),
+		(await import('@playform/compress')).default({
+			CSS: true,
+			HTML: false,
+			Image: false,
+			JavaScript: true,
+			SVG: true,
+		}),
+	],
+	site: 'http://localhost/',
 });
